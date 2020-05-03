38f5aeaee261d9ddbc9f57251cd22c8cd87de696
OM KUMAR YADAV <kumarom1203@gmail.com>
2020-05-03 23:49:55 +0530

    sudo systemctl start mariadb
    sudo systemctl start mariadb

--- LAMP_INSTALATION_CENT_OS_7.php
+++ LAMP_INSTALATION_CENT_OS_7.php
@@ -21,6 +21,8 @@ Step 3 -  Install MySQL (MariaDB) on CentOS 7
 
 sudo yum install mariadb-server mariadb  
 
+sudo systemctl start mariadb
+
 sudo mysql_secure_installation  
 
 -------------------- Some Question For-----------------------------
